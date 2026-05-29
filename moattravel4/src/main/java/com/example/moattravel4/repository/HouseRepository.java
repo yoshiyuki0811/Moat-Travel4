563a9085cd44d4e58b302bd04091b191b5c3be7d
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-29 16:30:34 +0900

    １６章削除機能実装まで

---
+++ moattravel4/src/main/java/com/example/moattravel4/repository/HouseRepository.java
@@ -0,0 +1,15 @@
+package com.example.moattravel4.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.moattravel4.Entity.House;
+
+@Repository
+public interface HouseRepository extends JpaRepository<House, Integer> {
+	
+	public Page<House> findByNameLike(String keyword, Pageable pageable);
+
+}
---
+++ moattravel4/src/main/java/com/example/moattravel4/service/HouseService.java
@@ -0,0 +1,120 @@
+package com.example.moattravel4.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.moattravel4.Entity.House;
+import com.example.moattravel4.form.HouseEditForm;
+import com.example.moattravel4.form.HouseRegisterForm;
+import com.example.moattravel4.repository.HouseRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class HouseService {
+	
+	private final HouseRepository houseRepository;
+	
+	@Transactional
+	public void create(HouseRegisterForm houseRegisterForm) {
+		
+		House house = new House();
+		
+		MultipartFile imageFile = houseRegisterForm.getImageFile();
+		
+		if(!imageFile.isEmpty()) {
+			
+			String imageName = imageFile.getOriginalFilename();
+			
+			String hashedImageName = generateNewFileName(imageName);
+			
+			Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+			
+			copyImageFile(imageFile, filePath);
+			
+			house.setImageName(hashedImageName);
+
+		}
+		house.setName(houseRegisterForm.getName());                
+		house.setDescription(houseRegisterForm.getDescription());
+		house.setPrice(houseRegisterForm.getPrice());
+		house.setCapacity(houseRegisterForm.getCapacity());
+		house.setPostalCode(houseRegisterForm.getPostalCode());
+		house.setAddress(houseRegisterForm.getAddress());
+		house.setPhoneNumber(houseRegisterForm.getPhoneNumber());
+		                    
+		houseRepository.save(house);
+		
+	}
+	@Transactional
+	public void update(HouseEditForm houseEditForm) {
+		
+		House house = houseRepository.getReferenceById(houseEditForm.getId());
+		
+		MultipartFile imageFile = houseEditForm.getImageFile();
+		
+		if(!imageFile.isEmpty()) {
+			
+			String imageName = imageFile.getOriginalFilename();
+			
+			String hashedImageName = generateNewFileName(imageName);
+			
+			Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+			
+			copyImageFile(imageFile, filePath);
+			
+			house.setImageName(hashedImageName);
+			
+			
+		}
+		house.setName(houseEditForm.getName());                
+		house.setDescription(houseEditForm.getDescription());
+		house.setPrice(houseEditForm.getPrice());
+		house.setCapacity(houseEditForm.getCapacity());
+		house.setPostalCode(houseEditForm.getPostalCode());
+		house.setAddress(houseEditForm.getAddress());
+		house.setPhoneNumber(houseEditForm.getPhoneNumber());
+		
+		houseRepository.save(house);
+		
+		
+		
+	}
+	
+	//UUIDを使って生成したファイル名を返す
+	public String generateNewFileName(String fileName) {
+		
+		String[] fileNames = fileName.split("\\.");
+		
+		for(int i =  0; i<fileNames.length -1; i++) {
+			
+			fileNames[i] = UUID.randomUUID().toString();
+			
+		}
+		
+		String hashedFileName = String.join(".", fileNames);
+		
+		return hashedFileName;
+	}
+	
+	//画像ファイルを指定したfileにコピーする
+	public void copyImageFile(MultipartFile imageFile, Path filePath) {
+		
+		try {
+			Files.copy(imageFile.getInputStream(), filePath);
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
