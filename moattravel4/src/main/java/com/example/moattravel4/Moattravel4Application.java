b0dd4e300188ac0b8445c4636eadb38b26539648
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-29 11:46:14 +0900

    ファーストコミット

---
+++ moattravel4/src/main/java/com/example/moattravel4/Moattravel4Application.java
@@ -0,0 +1,13 @@
+package com.example.moattravel4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moattravel4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moattravel4Application.class, args);
+	}
+
+}
---
+++ moattravel4/src/test/java/com/example/moattravel4/Moattravel4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moattravel4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moattravel4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
