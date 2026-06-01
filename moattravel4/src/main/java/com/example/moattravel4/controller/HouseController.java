b68717e8b6f40a05c377e125efce3448cf240f0f
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-06-01 16:37:14 +0900

    並び替え機能、ホーム画面の実装

--- moattravel4/src/main/java/com/example/moattravel4/controller/HouseController.java
+++ moattravel4/src/main/java/com/example/moattravel4/controller/HouseController.java
@@ -19,45 +19,80 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/houses")
 @RequiredArgsConstructor
 public class HouseController {
-	
+
 	private final HouseRepository houseRepository;
-	
+
 	@GetMapping
 	public String index(
-			@RequestParam(name = "keyword", required = false)String keyword,
-			@RequestParam(name = "area", required = false)String area,
-			@RequestParam(name = "price", required = false)Integer price,
-			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC)Pageable pageable,
-			Model model
-			) {
-		
+			@RequestParam(name = "keyword", required = false) String keyword,
+			@RequestParam(name = "area", required = false) String area,
+			@RequestParam(name = "price", required = false) Integer price,
+			@RequestParam(name = "order", required = false) String order,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			Model model) {
+
 		Page<House> housePage;
-		
-		if(keyword !=null && !keyword.isEmpty()) {
-			
+
+		if (keyword != null && !keyword.isEmpty()) {
+
 			housePage = houseRepository.findByNameLikeOrAddressLike("%" + keyword + "%", "%" + keyword + "%", pageable);
-		}else if(area != null && !area.isEmpty()) {
-			
+
+			if (order != null && order.equals("priceAsc")) {
+
+				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%",
+						"%" + keyword + "%", pageable);
+			} else {
+
+				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByCreatedAtDesc("%" + keyword + "%",
+						"%" + keyword + "%", pageable);
+			}
+		} else if (area != null && !area.isEmpty()) {
+
 			housePage = houseRepository.findByAddressLike("%" + area + "%", pageable);
-		}else if(price !=null) {
-			
+
+			if (order != null && order.equals("priceAsc")) {
+
+				housePage = houseRepository.findByAddressLikeOrderByPriceAsc("%" + area + "%", pageable);
+
+			} else {
+
+				housePage = houseRepository.findByAddressLikeOrderByCreatedAtDesc("%" + area + "%", pageable);
+			}
+
+		} else if (price != null) {
+
 			housePage = houseRepository.findByPriceLessThanEqual(price, pageable);
-		}else {
 			
+			if(order !=null && order.equals("priceAsc")) {
+				
+				housePage = houseRepository.findByPriceLessThanEqualOrderByPriceAsc(price, pageable);
+			}else {
+				
+				housePage = houseRepository.findByPriceLessThanEqualOrderByCreatedAtDesc(price, pageable);
+			}
+		} else {
+
 			housePage = houseRepository.findAll(pageable);
+			
+			if(order !=null && order.equals("priceAsc")) {
+				
+				housePage = houseRepository.findAllByOrderByPriceAsc(pageable);
+			}else {
+				housePage =houseRepository.findAllByOrderByCreatedAtDesc(pageable);
+			}
 		}
-		
+
 		model.addAttribute("housePage", housePage);
-		
+
 		model.addAttribute("keyword", keyword);
-		
+
 		model.addAttribute("area", area);
-		
+
 		model.addAttribute("price", price);
 		
+		model.addAttribute("order", order);
+
 		return "houses/index";
 	}
-	
-	
 
 }
--- moattravel4/src/main/java/com/example/moattravel4/event/SignupEventListener.java
+++ moattravel4/src/main/java/com/example/moattravel4/event/SignupEventListener.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class signupEventListener {
+public class SignupEventListener {
 
 	private final VerificationTokenService verificationTokenService;
 	
